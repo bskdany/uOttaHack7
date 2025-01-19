664e4406a7510e821cc6ab104d4b67beea973c84
bskdany <online@bskdany.com>
2025-01-19 02:24:25 -0500

    movement

--- qnx_main.py
+++ qnx_main.py
@@ -1,7 +1,7 @@
 import rpi_gpio as GPIO
 import time
 
-
+#17, 27 
 sensor_input = [17,27,22,5,6,26,25,16]
 movement_output = [23,24]
 
@@ -18,4 +18,23 @@ while True:
     
     time.sleep(1)
     print(input_data)
+    if (input_data[0] == 0 and input_data[1] == 0):
+        print("Forward")
+        GPIO.output(movement_output[0], GPIO.HIGH)
+        GPIO.output(movement_output[1], GPIO.HIGH)
+    elif (input_data[0] == 0 and input_data[1] == 1):
+        print("Right")
+        GPIO.output(movement_output[0], GPIO.HIGH)
+        GPIO.output(movement_output[1], GPIO.LOW)
+    elif (input_data[0] == 1 and input_data[1] == 0):
+        print("stop? back?")
+        GPIO.output(movement_output[0], GPIO.LOW)
+        GPIO.output(movement_output[1], GPIO.LOW)
+    elif (input_data[0] == 1 and input_data[1] == 1):
+        print("Left")
+        GPIO.output(movement_output[0], GPIO.LOW)
+        GPIO.output(movement_output[1], GPIO.HIGH)
+
+
+
 
