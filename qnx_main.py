e6d13bfb34193a682c43d960140a6e47de68d405
bskdany <online@bskdany.com>
2025-01-19 05:14:51 -0500

    done?

--- qnx_main.py
+++ qnx_main.py
@@ -11,6 +11,34 @@ for i in sensor_input:
 for i in movement_output:
     GPIO.setup(i, GPIO.OUT)
 
+latest_data = {
+    "front_sensor": -1,
+    "left_sensor": -1,
+    "right_sensor": -1,
+    "back_sensor": -1,
+}
+
+def moveForward():
+    print("Moving forward")
+    GPIO.output(movement_output[0], GPIO.HIGH)
+    GPIO.output(movement_output[1], GPIO.HIGH)
+
+def rotateRight():
+    print("Rotating right")
+    GPIO.output(movement_output[0], GPIO.HIGH)
+    GPIO.output(movement_output[1], GPIO.LOW)
+
+def rotateLeft():
+    print("Rotating left")
+    GPIO.output(movement_output[0], GPIO.LOW)
+    GPIO.output(movement_output[1], GPIO.HIGH)
+
+def stopRumba():
+    print("Stopping")
+    GPIO.output(movement_output[0], GPIO.LOW)
+    GPIO.output(movement_output[1], GPIO.LOW)
+
+
 while True:
     input_data = []
     for i in sensor_input:
@@ -18,22 +46,54 @@ while True:
     
     time.sleep(1)
     print(input_data)
+    sixBits = ""
+    
+    for i in input_data[2:]:
+        sixBits += str(i)
+    
+    sixBits+="000"
+    decimal = int(sixBits, 2)
+    print(decimal)
+    
     if (input_data[0] == 0 and input_data[1] == 0):
         print("Forward")
-        GPIO.output(movement_output[0], GPIO.HIGH)
-        GPIO.output(movement_output[1], GPIO.HIGH)
+        latest_data["front_sensor"] = decimal
+        # GPIO.output(movement_output[0], GPIO.HIGH)
+        # GPIO.output(movement_output[1], GPIO.HIGH)
     elif (input_data[0] == 0 and input_data[1] == 1):
         print("Right")
-        GPIO.output(movement_output[0], GPIO.HIGH)
-        GPIO.output(movement_output[1], GPIO.LOW)
+        latest_data["right_sensor"] = decimal
+        # GPIO.output(movement_output[0], GPIO.HIGH)
+        # GPIO.output(movement_output[1], GPIO.LOW)
     elif (input_data[0] == 1 and input_data[1] == 0):
         print("stop? back?")
-        GPIO.output(movement_output[0], GPIO.LOW)
-        GPIO.output(movement_output[1], GPIO.LOW)
+        latest_data["back_sensor"] = decimal
+        # GPIO.output(movement_output[0], GPIO.LOW)
+        # GPIO.output(movement_output[1], GPIO.LOW)
     elif (input_data[0] == 1 and input_data[1] == 1):
         print("Left")
-        GPIO.output(movement_output[0], GPIO.LOW)
-        GPIO.output(movement_output[1], GPIO.HIGH)
+        latest_data["left_sensor"] = decimal
+        # GPIO.output(movement_output[0], GPIO.LOW)
+        # GPIO.output(movement_output[1], GPIO.HIGH)
+    
+    max_direction = max(latest_data, key=latest_data.get)
+    print(max_direction)
+
+
+    if (latest_data["front_sensor"] - latest_data["back_sensor"] < 50) and (latest_data["right_sensor"] - latest_data["left_sensor"] < 50):
+        stopRumba()
+    elif max_direction == "front_sensor":
+        moveForward()
+    elif max_direction == "right_sensor":
+        rotateRight()
+    elif max_direction == "left_sensor":
+        rotateLeft()
+    elif max_direction == "back_sensor":
+        rotateRight()
+
+
+    
+    # print(sixBits)
 
 
 
