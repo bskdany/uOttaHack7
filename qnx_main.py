9081189c753838457fd7a8d23848ca665d033eef
bskdany <online@bskdany.com>
2025-01-19 00:56:39 -0500

    setting up the pins

--- qnx_main.py
+++ qnx_main.py
@@ -1,13 +1,21 @@
 import rpi_gpio as GPIO
 import time
-import rpi_gpio as GPIO
-import time
 
-GPIO.setup(16, GPIO.OUT)
-GPIO.output(16, GPIO.LOW)
+
+sensor_input = [17,27,22,5,6,26,25,16]
+movement_output = [23,24]
+
+for i in sensor_input:
+    GPIO.setup(i, GPIO.IN)
+
+for i in movement_output:
+    GPIO.setup(i, GPIO.OUT)
 
 while True:
-    GPIO.output(16, GPIO.HIGH)
-    time.sleep(.5)
-    GPIO.output(16, GPIO.LOW)
-    time.sleep(.5)
+    input_data = []
+    for i in sensor_input:
+        input_data.append(GPIO.input(i))
+    
+    time.sleep(1)
+    print(input_data)
+
