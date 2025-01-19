0873f7cbb78200ce28af1332e381ee2dfcd5b836
isharghura <ishar.ghuras@gmail.com>
2025-01-19 00:33:26 -0500

    Removed pi

--- gpio_comm/pi/pi_gpio_comm.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define NUM_PINS 10
-
-const int OUTPUT_PINS[NUM_PINS] = {23, 24, 25, 16};
-const int INPUT_PINS[NUM_PINS] = {17, 27, 22, 5, 6, 26};
